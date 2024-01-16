75402045f6760654c63ec7d7fb3bfdd85d3e234c
yonghyeonpark <pyh4616@naver.com>
2024-01-17 03:35:14 +0900

    스프링 인터셉터 - 요청 로그

---
+++ src/main/java/mvc2/login/WebConfig.java
@@ -0,0 +1,18 @@
+package mvc2.login;
+
+import mvc2.login.web.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
+    }
+}
